b02c3e0748ff77300dd83a9884214b58319908e9
BibaltiK <bibaltik@exdrals.de>
2024-11-01 12:28:21 +0100

    add home link

--- src/components/NavigationHeadBar.vue
+++ src/components/NavigationHeadBar.vue
@@ -1,18 +1,17 @@
 <script setup>
-
 </script>
 
 <template>
   <nav class="navbar navbar-expand-sm navbar-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#"><i class="bi bi-house-door-fill h2"></i></a>
+      <router-link :to="{name: 'home'}" class="navbar-brand"><i class="bi bi-house-door-fill h2"></i></router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-sm-0">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
+            <router-link :to="{name: 'home'}" class="nav-link" aria-current="page">Home</router-link>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
