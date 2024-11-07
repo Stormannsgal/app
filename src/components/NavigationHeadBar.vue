b1615aa5f2f186081fa6403bffb8fae8e229f020
BibaltiK <github@exdrals.de>
2024-11-07 11:33:18 +0100

    Revert "add home link"
    This reverts commit 0cf50fcef9bd92212e6428a483ab5baba0781a43.

--- src/components/NavigationHeadBar.vue
+++ src/components/NavigationHeadBar.vue
@@ -1,17 +1,18 @@
 <script setup>
+
 </script>
 
 <template>
   <nav class="navbar navbar-expand-sm navbar-dark">
     <div class="container-fluid">
-      <router-link :to="{name: 'home'}" class="navbar-brand"><i class="bi bi-house-door-fill h2"></i></router-link>
+      <a class="navbar-brand" href="#"><i class="bi bi-house-door-fill h2"></i></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-sm-0">
           <li class="nav-item">
-            <router-link :to="{name: 'home'}" class="nav-link" aria-current="page">Home</router-link>
+            <a class="nav-link" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
