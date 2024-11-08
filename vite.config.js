7d98185836e9d250d22965471d1bda2c80981697
BibaltiK <bibaltik@exdrals.de>
2024-11-08 08:51:48 +0100

    add compression

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import viteCompression from 'vite-plugin-compression2';
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -12,6 +13,7 @@ export default defineConfig({
   publicDir: 'assets',
   plugins: [
     vue(),
+    viteCompression({algorithm: 'gzip'}),
   ],
   resolve: {
     alias: {
