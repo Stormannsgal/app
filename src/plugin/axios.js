c28be5d9017c936719b65573b31d53793669f098
BibaltiK <bibaltik@exdrals.de>
2024-11-10 18:37:03 +0100

    --

--- src/plugin/axios.js
+++ src/plugin/axios.js
@@ -3,6 +3,7 @@ import router from "@/router";
 import {useTokenStore} from "@/stores/TokenStore.js";
 
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+console.log(import.meta.env.VITE_API_BASE_URL)
 axios.defaults.withCredentials = true;
 
 const clientID = localStorage.getItem('uniqueID')
